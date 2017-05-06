fa7c4f90c97821c1c886393363b62168afeb5e30
Jupiter-Lee <450663973@qq.com>
2017-05-06 16:38:43 +0800

    提交代码

--- app/index.jsx
+++ app/index.jsx
@@ -2,24 +2,51 @@ import './index.less';
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-import { Input } from 'antd';
+import { Input, Form, Button } from 'antd';
 
+const FormItem = Form.Item;
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+        if (!err) {
+            console.log('Received values of form: ', values);
+        }
+        });
+    }
+
     render() {
+        const { getFieldDecorator } = this.props.form;
         return (
-            <div>
-                Hello, FCC.
-                <br />
-                <Input 
-                    style={{ width: 200 }}
-                    placeholder="fcc" 
-                />
+            <div className="login-container">
+                <Form onSubmit={this.handleSubmit}>
+                    <FormItem>
+                        {getFieldDecorator('userName', {
+                            rules: [{ required: true, message: 'Please input your username!' }]
+                        })(
+                            <Input placeholder="Username" />
+                        )}
+                    </FormItem>
+                    <FormItem>
+                        {getFieldDecorator('password', {
+                            rules: [{ required: true, message: 'Please input your password!' }]
+                        })(
+                            <Input type="password" placeholder="password" />
+                        )}
+                    </FormItem>
+                    <Button htmlType="submit">提交</Button>
+                </Form>
             </div>
         );
     }
 };
-
+const LoginForm = Form.create()(App);
 render(
-    <App />,
+    <LoginForm />,
     document.getElementById('app')
 );
